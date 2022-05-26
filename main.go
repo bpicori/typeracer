2c13c81eab5c85c7ac61a0f9875f7f36df08c775
Besnik Picori <picori.besnik@gmail.com>
2022-05-26 11:07:42 +0200

    init layout

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-// Demo code for the TextView primitive.
 package main
 
 import (
@@ -36,27 +35,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err := ioutil.ReadFile(config.filePath)
+	b, err := ioutil.ReadFile(config.filePath)
 	if err != nil {
 		fmt.Print("error reading file: ", err)
 		os.Exit(1)
 	}
 
-	// fileContent := string(b)
+	fileContent := string(b)
 	cursor := `["cursor"] [""]`
 
 	app := tview.NewApplication()
-	textView := tview.NewTextView().
-		SetDynamicColors(true).
-		SetRegions(true).
-		SetWordWrap(true).
-		SetScrollable(true).
-		SetText(cursor).
-		Highlight("cursor").
-		SetToggleHighlights(true).
-		SetChangedFunc(func() {
-			app.Draw()
-		})
+
+	fileView := getFileView(app, fileContent, cursor, config.filePath)
+	typeView := getFileView(app, "", "", "Type..")
 
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyESC {
@@ -64,35 +55,63 @@ func main() {
 		}
 		if event.Key() == tcell.KeyRune {
 			key := string(event.Rune())
-			text := textView.GetText(false)
+			text := fileView.GetText(false)
 			newText := strings.ReplaceAll(text, cursor, "")
 			newText = newText[:len(newText)-1]
-			textView.Clear()
-			fmt.Fprintf(textView, "%s", newText+key+cursor)
+			fileView.Clear()
+			fmt.Fprintf(fileView, "%s", newText+key+cursor)
 		}
 
 		if event.Key() == tcell.KeyEnter {
 			key := "\n"
-			text := textView.GetText(false)
+			text := fileView.GetText(false)
 			newText := strings.ReplaceAll(text, cursor, "")
 			newText = newText[:len(newText)-1]
-			textView.Clear()
-			fmt.Fprintf(textView, "%s", newText+key+cursor)
+			fileView.Clear()
+			fmt.Fprintf(fileView, "%s", newText+key+cursor)
 		}
 
 		if event.Key() == tcell.KeyBackspace || event.Key() == tcell.KeyBackspace2 {
-			text := textView.GetText(false)
+			text := fileView.GetText(false)
 			newText := strings.ReplaceAll(text, cursor, "")
-			newText = newText[:len(newText)-2]
-			textView.Clear()
-			fmt.Fprintf(textView, "%s", newText+cursor)
+			if (len(newText) - 2) < 0 {
+				fileView.Clear()
+				fmt.Fprintf(fileView, "%s", cursor)
+			} else {
+				newText = newText[:len(newText)-2]
+				fileView.Clear()
+				fmt.Fprintf(fileView, "%s", newText+cursor)
+			}
 		}
 
 		return event
 	})
 
-	textView.SetBorder(true)
-	if err := app.SetRoot(textView, true).EnableMouse(true).Run(); err != nil {
+	flex := tview.NewFlex().
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(fileView, 0, 1, false).
+			AddItem(typeView, 0, 1, false), 0, 2, false).
+		AddItem(tview.NewBox().SetBorder(true).SetBorderColor(tcell.ColorOlive).SetTitle("Right (20 cols)"), 30, 1, false)
+
+	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
+
+}
+
+func getFileView(app *tview.Application, fileContent string, cursor string, title string) *tview.TextView {
+	fileView := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetWordWrap(true).
+		SetScrollable(true).
+		SetText(fileContent + cursor).
+		Highlight("cursor").
+		SetToggleHighlights(true).
+		SetChangedFunc(func() {
+			app.Draw()
+		})
+	fileView.SetTitle(title).SetBorder(true).SetBorderColor(tcell.ColorOliveDrab)
+
+	return fileView
 }
